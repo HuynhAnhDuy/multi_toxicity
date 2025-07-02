b6712fbee0c49493bb6628e6841e952e3a175abc
HuynhAnhDuy <huynhduyds@gmail.com>
2025-07-02 11:24:58 +0700

    update

--- app.py
+++ app.py
@@ -5,16 +5,38 @@ from model_utils import load_models, predict_all
 st.set_page_config(page_title="Toxicity Predictor", layout="centered")
 st.title("☣️ Multi-endpoint Toxicity Predictor")
 
+st.markdown("""
+Predict the toxicity potential of chemical compounds using machine learning models with 8 various endpoints.
+
+**Developers:**  
+**Affiliations:** 
+""", unsafe_allow_html=True)
+
+# === Sidebar Instructions ===
+with st.sidebar:
+    st.header("🧾 Instructions")
+    st.markdown("""
+    1. Paste a SMILES string.
+    2. Click **Predict**.    
+    """)
+
+    st.markdown("---")
+    st.markdown("""
+    🔍 **Prediction Rule:**  
+    - **Avg. probability > 0.5** → ☣️ **Possible toxicity** 
+    - **Avg. probability ≤ 0.5** → ✅ **Non-toxicity**
+    """)
+
 # Tên đầy đủ của 8 endpoint
 ENDPOINT_NAMES = [
-    "Mutagenicity",
-    "Carcinogenicity",
-    "hERG Inhibition",
+    "Peripheral blood mononuclear cells toxicity",
+    "Nephrotoxicity",
+    "Neurotoxicity",
     "Hepatotoxicity",
     "Skin Sensitization",
     "Respiratory Toxicity",
-    "Developmental Toxicity",
-    "Neurotoxicity"
+    "Severe Cutaneous Adverse Reaction",
+    "Cardiotoxicity"
 ]
 
 # Load mô hình & file descriptor XML
